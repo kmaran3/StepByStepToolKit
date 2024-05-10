8c69eb977be74c8bda2dc0c42aa33f77081e983a
Deven <deven@chatterjea.com>
2024-05-10 01:24:35 -0400

    Final Test Push - 5/9

--- webapp/views.py
+++ webapp/views.py
@@ -17,24 +17,4 @@ def contact():
 
 @main.route('/rankings')
 def rankings():
-    return render_template('rankings.html', players=df.to_dict('records'))
-
-# Sample data for player rankings
-data = {'Player': ['Player 1', 'Player 2', 'Player 3'], 'Rank': [1, 2, 3]}
-df = pd.DataFrame(data)
-
-# Endpoint to handle move up and move down
-@main.route('/move', methods=['POST'])
-def move_player():
-    player_name = request.json['player']
-    direction = request.json['direction']
-
-    # Find the index of the player
-    index = df.index[df['Player'] == player_name].tolist()[0]
-
-    if direction == 'up' and index > 0:
-        df.loc[index-1], df.loc[index] = df.loc[index], df.loc[index-1]
-    elif direction == 'down' and index < len(df) - 1:
-        df.loc[index+1], df.loc[index] = df.loc[index], df.loc[index+1]
-
-    return jsonify(success=True, players=df.to_dict('records'))
+    return render_template('rankings.html')
