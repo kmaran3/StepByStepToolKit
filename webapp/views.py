a70bc775366c88d7bb6ea3ff0c7891499b67131d
Deven <deven@chatterjea.com>
2024-05-22 15:39:43 -0400

    Mockdraft Tab Incorperated. Draft Pick Selector Scrolldown. Ranking Generated after Start Draft Button Clicked.

--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
@@ -48,3 +48,34 @@ def rankings():
         html_data = "<p>Failed to retrieve data.</p>"
 
     return render_template('rankings.html', table=html_data)
+
+@main.route('/mockdraft', methods=['GET', 'POST'])
+def mock_draft():
+    if request.method == 'POST':
+        draft_position = request.form['position']
+        player_data = fetch_player_data()  # Call function to fetch data
+        return render_template('mockdraft.html', player_data=player_data, draft_position=draft_position)
+    else:
+        return render_template('mockdraft.html')
+
+def fetch_player_data():
+    url = 'https://www.footballguys.com/adp'
+    response = requests.get(url)
+    player_data = []
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        rows = soup.find_all('tr')
+        for row in rows:
+            name_td = row.find('td', class_='name sticky-col text-start')
+            if name_td:
+                name_a = name_td.find('a')
+                if name_a:
+                    player_name = name_a.get_text().strip()
+                    tds = row.find_all('td')
+                    if len(tds) >= 9:
+                        espn_value = tds[8].get_text().strip()
+                        player_data.append((player_name, espn_value))
+        player_data_sorted = sorted(player_data, key=lambda x: int(x[1]) if x[1].isdigit() else float('inf'))
+        return player_data_sorted
+    else:
+        return []
